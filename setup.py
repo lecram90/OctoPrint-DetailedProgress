61309a614bdb9a15db58c5787fd3ba9d5ab8a6ab
lecram90 <lecram90@users.noreply.github.com>
2019-02-13 01:27:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Dattas Moonchaser"
 plugin_author_email = "dattasmoon@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/dattas/OctoPrint-DetailedProgress"
+plugin_url = "https://github.com/lecram90/OctoPrint-DetailedProgress"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
